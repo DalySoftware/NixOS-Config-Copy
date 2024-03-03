4821d7fb489e216e46e9c19b29e9f06b5aa39347
Chris <irondoctorchris@gmail.com>
2024-03-03 21:58:13 +0000

    Add real rebuild script

--- modules/rebuild-script.nix
+++ modules/rebuild-script.nix
@@ -1,7 +1,39 @@
-{ pkgs, ... }: 
-let script = pkgs.writeShellScriptBin "rebuild" ''
-      echo "rebuilding!"
-    '';
+{pkgs, ...}: let
+  script = pkgs.writeShellScriptBin "rebuild" ''
+    set -e
+
+    # cd to your config dir
+    pushd ~/.nix-config/
+
+    # Autoformat your nix files
+    alejandra . -q
+
+    # Shows your changes
+    git diff -U0 *.nix
+
+    # Stage all changes so they are included in rebuild
+    git add .
+
+    echo "NixOS Rebuilding..."
+
+    # Rebuild, output simplified errors, log tracebacks
+    sudo nixos-rebuild switch --flake .#default &>nixos-switch.log || (cat nixos-switch.log | grep --color error && false)
+
+    # Commit all changes prompting for message
+    git commit -a
+
+    if [ "$?" -ne 0 ]; then
+        echo Cancelling...
+        popd
+        exit 1
+    fi
+
+    # Back to where you were
+    popd
+
+    # Notify all OK!
+    # notify-send -e "NixOS Rebuilt OK!" --icon=software-update-available
+  '';
 in {
-  environment.systemPackages = [ script ];
+  environment.systemPackages = [script];
 }
