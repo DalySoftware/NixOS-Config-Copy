dd9867ba7da0e769636f02e4c22afce0befbfb56
Chris <irondoctorchris@gmail.com>
2024-03-03 17:39:07 +0000

    Initial commit. After vimjoyer basic setup.

---
+++ flake.nix
@@ -0,0 +1,31 @@
+
+{
+  description = "Nixos config flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
+    nixos-wsl.url = "github:nix-community/nixos-wsl";
+
+    # home-manager = {
+    #   url = "github:nix-community/home-manager";
+    #   inputs.nixpkgs.follows = "nixpkgs";
+    # };
+  };
+
+  outputs = { self, nixpkgs, nixos-wsl, ... }@inputs:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in
+    {
+      nixosConfigurations.default = nixpkgs.lib.nixosSystem {
+        specialArgs = {inherit inputs;};
+        system = system;
+        modules = [ 
+          ./configuration.nix
+          nixos-wsl.nixosModules.wsl
+          # inputs.home-manager.nixosModules.default
+        ];
+      };
+  };
+}
