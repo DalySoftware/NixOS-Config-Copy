cc3d32a924690a5f93c90870e50ab4d3aba0b002
Chris <irondoctorchris@gmail.com>
2024-03-06 21:50:54 +0000

    Fix hstr config and remove keyboard shortcut

--- modules/shell/shell.nix
+++ modules/shell/shell.nix
@@ -1,13 +1,13 @@
-{pkgs, ...}: let
+{
+  pkgs,
+  lib,
+  ...
+}: let
   ompTheme = ./pure_custom.omp.json;
 in {
   programs.zsh = {
     enable = true;
     enableAutosuggestions = true;
-    initExtra = ''
-      export HSTR_CONFIG="prompt-bottom,raw-history-view,hicolor";
-      bindkey -s "$terminfo[kcuu1]" "\C-a hstr -- \C-j";
-    '';
     completionInit = ''
       autoload -Uz compinit && compinit
       zstyle ':completion:*' matcher-list 'm:{a-z}={A-Za-z}'
@@ -17,14 +17,17 @@ in {
   programs.zoxide.enable = true;
   programs.zoxide.enableZshIntegration = true;
 
-  programs.hstr.enable = true;
-  programs.hstr.enableZshIntegration = true;
-
   programs.oh-my-posh = {
     enable = true;
     enableZshIntegration = true;
     settings = builtins.fromJSON (builtins.readFile ompTheme);
   };
+
+  programs.hstr.enable = true;
+  programs.hstr.enableZshIntegration = lib.mkAfter true;
+  programs.zsh.initExtra = lib.mkAfter ''
+    export HSTR_CONFIG="prompt-bottom,raw-history-view,hicolor";
+  '';
 
   home.packages = with pkgs; [
     nerdfonts
